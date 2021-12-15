60b5e01e4bd82e873986a3fb504a9077a164ecbe
sifatulrabbi <mdsifatulislam.rabbi@gmail.com>
2021-12-15 10:46:28 +0600

    fix(types): fix method return types in bmi.py

--- bmi_bmr_calculator/bmi.py
+++ bmi_bmr_calculator/bmi.py
@@ -8,19 +8,19 @@ class BMICalculator:
         self.__converter = Converter()
         pass
 
-    def __calculate(self, height: float, weight: float) -> float:
+    def __calculate(self, height: float, weight: float) -> None:
         bmi = weight / (height * height)
         formated_bmi = "{:.2f}".format(bmi)
 
         print(f"\nYour BMI is: {formated_bmi}")
 
-    def cm_lb(self, height: float, weight: float):
+    def cm_lb(self, height: float, weight: float) -> None:
         meter_height = self.__converter.cm_to_meter(height)
         kg_weight = self.__converter.lb_to_kg(weight)
 
         return self.__calculate(meter_height, kg_weight)
 
-    def inch_kg(self, height: float, weight: float):
+    def inch_kg(self, height: float, weight: float) -> None:
         meter_height = self.__converter.inch_to_meter(height)
 
         return self.__calculate(meter_height, weight)
