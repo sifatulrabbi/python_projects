d4da14de7f52d5d27ed2873d6b3fc6c2abfa2c25
Md Sifatul Islam Rabbi <mdsifatulislam.rabbi@gmail.com>
2022-03-06 22:28:40 +0600

    build: hangman_game

---
+++ hangman-game/main.py
@@ -0,0 +1,20 @@
+import random
+import words
+import display_hangman
+
+
+# Get a random word
+
+# Show underscores instead of letters
+
+# take in guessed letter
+
+# check if the letter is used before
+
+# check if the letter is in the word
+
+# if the letter is in the word then reveal the letter and prompt for the next user input
+
+# else show hangman if the user still has tries then prompt for another user input
+
+# if the letter completes the word then show the word and end the game
